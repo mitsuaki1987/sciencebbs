d05d00d9b7a4fa669de22aba22ca4c9f71b6eca1
Mitsuaki Kawamura <kawamitsuaki@gmail.com>
2020-08-04 16:14:04 +0900

    Backup

--- draw.js
+++ draw.js
@@ -11,16 +11,13 @@ var ongoingTouches = [];
 
 function handleStart(evt) {
     evt.preventDefault();
-    log("touchstart.");
     var el = document.getElementById("canvas");
     var ctx = el.getContext("2d");
     var touches = evt.changedTouches;
     
     for (var i = 0; i < touches.length; i++) {
-        log("touchstart:" + i + "...");
         ongoingTouches.push(copyTouch(touches[i]));
         var color = colorForTouch(touches[i]);
-        log("touchstart:" + i + ".");
     }
 }
 
@@ -36,27 +33,20 @@ function handleMove(evt) {
         var idx = ongoingTouchIndexById(touches[i].identifier);
 
         if (idx == 0) {
-            log("continuing touch "+idx);
             ctx.beginPath();
-            log("ctx.moveTo(" + ongoingTouches[idx].clientX + ", " + ongoingTouches[idx].clientY + ");");
             ctx.moveTo(ongoingTouches[idx].clientX -  rect.left, ongoingTouches[idx].clientY - rect.top);
-            log("ctx.lineTo(" + touches[i].clientX + ", " + touches[i].clientY + ");");
-            log("ctx.lineTo2(" + rect.left + ", " + rect.top + ");");
             ctx.lineTo(touches[i].clientX - rect.left, touches[i].clientY - rect.top);
             ctx.lineWidth = 4;
             ctx.strokeStyle = color;
             ctx.stroke();
 
             ongoingTouches.splice(idx, 1, copyTouch(touches[i]));  // swap in the new touch record
-            log(".");
         } else {
-            log("can't figure out which touch to continue");
         }
     }
 }
 function handleEnd(evt) {
     evt.preventDefault();
-    log("touchend");
     var el = document.getElementById("canvas");
     var ctx = el.getContext("2d");
     var touches = evt.changedTouches;
@@ -68,13 +58,11 @@ function handleEnd(evt) {
         if (idx >= 0) {
             ongoingTouches.splice(idx, 1);  // remove it; we're done
         } else {
-            log("can't figure out which touch to end");
         }
     }
 }
 function handleCancel(evt) {
     evt.preventDefault();
-    log("touchcancel.");
     var touches = evt.changedTouches;
     
     for (var i = 0; i < touches.length; i++) {
@@ -90,7 +78,6 @@ function colorForTouch(touch) {
     g = g.toString(16); // make it a hex digit
     b = b.toString(16); // make it a hex digit
     var color = "#" + r + g + b;
-    log("color for touch with identifier " + touch.identifier + " = " + color);
     return color;
 }
 function copyTouch({ identifier, clientX, clientY }) {
@@ -106,8 +93,4 @@ function ongoingTouchIndexById(idToFind) {
         }
     }
     return -1;    // not found
-}
-function log(msg) {
-    var p = document.getElementById('log');
-    p.innerHTML = msg + "\n" + p.innerHTML;
 }
--- index.php
+++ index.php
@@ -1,50 +1,72 @@
 <?php
   $fp = fopen('data.csv', 'a+b');
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  fputcsv($fp, [$_POST['name'], $_POST['comment']]);
-  rewind($fp);
+    fputcsv($fp, [$_POST['name'], $_POST['comment'], $_POST['submittype']]);
+    rewind($fp);
   }
   while ($row = fgetcsv($fp)) {
-  $rows[] = $row;
+    $rows[] = $row;
   }
   fclose($fp);
-  ?>
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" 
+          content="text/html; charset=utf-8">
+    <script type="text/x-mathjax-config">
+      MathJax.Hub.Config({ tex2jax: { inlineMath: [['$','$'], ["\\(","\\)"]] } });
+    </script>
+    <script type="text/javascript"
+            src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML">
+    </script>
+    <meta http-equiv="X-UA-Compatible" CONTENT="IE=EmulateIE7" />
     <title>BBS</title>
   </head>
   <body>
     <h1>BBS</h1>
-    <section>
-    </section>
-    <section class="toukou">
-      <h2>Comments</h2>
-      <?php if (!empty($rows)): ?>
+
+    <h2>Comments</h2>
+    <?php if (!empty($rows)): ?>
       <ul>
         <?php foreach ($rows as $row): ?>
-        <li><?=$row[1]?> (<?=$row[0]?>)</li>
+          <?php if ($row[2] == 'text'): ?>
+            <li><?=$row[1]?> (<?=$row[2]?>) (by <?=$row[0]?>)</li>
+          <?php elseif ($row[2] == 'latex'): ?>
+            <li>\[ <?=$row[1]?> \] (<?=$row[2]?>) (by2  <?=$row[0]?>)</li>
+          <?php endif; ?>
         <?php endforeach; ?>
       </ul>
-      <?php else: ?>
+    <?php else: ?>
       <p>No comments</p>
-      <?php endif; ?>
-    </section>
-      <h2>Submit</h2>
-      <form action="" method="post">
-        <div class="name">
-          <span class="label">Name:</span><input type="text" name="name" value="">
-        </div>
-        <div class="honbun">
-          <span class="label">Comment:</span>
-          <textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard" placeholder="Within 80 latters">
-          </textarea>
-        </div>
-        <canvas id="canvas" width="600" height="600" style="border:solid black 1px;"></canvas>
-        <pre id="log" style="border: 1px solid #ccc;"></pre>
-        <script src="./draw.js"></script>
-        <input type="submit" value="Submit">
-      </form>
+    <?php endif; ?>
+
+    <h2>Submit</h2>
+    
+    <form action="" method="post">
+      Name: <input type="text" name="name" value="">
+      </br>
+      Comment:
+      </br>
+      <textarea name="comment" cols="30" rows="3" maxlength="80" wrap="hard" placeholder="Within 80 latters">
+      </textarea>
+      </br>
+      File: <input type="file" name="filename">
+      <br>
+      Free hand:
+      </br>
+      <canvas id="canvas" width="600" height="600" style="border:solid black 1px;"></canvas>
+      <script src="./draw.js"></script>
+      <button onclick="startup()">Initialize</button>
+      <br>
+      <input type="radio" name="submittype" value="text" checked="checked">Text
+      <input type="radio" name="submittype" value="latex">Latex
+      <input type="radio" name="submittype" value="freehand">FreeHand
+      <input type="radio" name="submittype" value="image">Image
+      <input type="radio" name="submittype" value="file">File
+      <input type="submit" value="Submit">
+    </form>
+    <hr>
   </body>
 </html>
